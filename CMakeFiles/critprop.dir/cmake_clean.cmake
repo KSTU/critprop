16a919bdad4c00d6de83af12de8a8364344bd6f1
Ivan <anashkin.ivan@gmail.com>
2014-12-16 22:29:12 +0300

    add all files

---
+++ CMakeFiles/critprop.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/critprop.dir/main.c.o"
+  "critprop.pdb"
+  "critprop"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/critprop.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
